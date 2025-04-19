6d4d93b7a92c6f9375ce13462d0cb87c287a47a4
itayyamin <itayyamin@microsoft.com>
2025-04-19 10:56:43 +0300

    add exmaples

--- qustions/SocialNetwork.java
+++ qustions/SocialNetwork.java
@@ -8,6 +8,11 @@ public class SocialNetwork {
             {0, 0, 0, 0, 0}
     };
 
+    // Returns an array containing all the users that the given user follows
+    public static int[] whoUserFollows(int user){
+        return null; // Placeholder
+    }
+
 
     //Returns the number of followers that the given user has
     public static int nFollowers(int user){
@@ -15,7 +20,7 @@ public class SocialNetwork {
     };
 
     //Returns the user that has the largest number of followers
-    public static int mistPopularUser(){
+    public static int mostPopularUser(){
         return -1;
     };
 
@@ -23,7 +28,30 @@ public class SocialNetwork {
     //The array's length is the number of users that the given user follows
 
     public static void main(String[] args) {
+        System.out.println("User 1 has " + nFollowers(1) + " followers.");
+        System.out.println("User 2 has " + nFollowers(2) + " followers.");
+        System.out.println("User 3 has " + nFollowers(3) + " followers.");
 
+        int popularUser = mostPopularUser();
+        System.out.println("The most popular user is: User " + popularUser);
+
+        int[] follows0 = whoUserFollows(0);
+        System.out.print("User 0 follows: ");
+        if (follows0 != null) {
+            for (int user : follows0) {
+                System.out.print(user + " ");
+            }
+        }
+        System.out.println();
+
+        int[] follows2 = whoUserFollows(2);
+        System.out.print("User 2 follows: ");
+        if (follows2 != null) {
+            for (int user : follows2) {
+                System.out.print(user + " ");
+            }
+        }
+        System.out.println();
     }
 
 
